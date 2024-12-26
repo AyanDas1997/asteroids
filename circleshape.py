0c98e28d0663ea971044b180d292829691aa8073
AyanDas1997 <das.ayandas.ayan@gmail.com>
2024-12-26 20:42:35 +0530

    All

--- circleshape.py
+++ circleshape.py
@@ -15,3 +15,8 @@ class CircleShape(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
+
+    def has_collided(self, other):
+          return pygame.math.Vector2.distance_to(self.position, other.position) <= self.radius + other.radius
+
+    
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -8,7 +11,11 @@ def main():
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while(True):
@@ -17,6 +24,9 @@ def main():
                 return
         for obj in updatable:
             obj.update(dt)
+        for asteroid in asteroids:
+            if asteroid.has_collided(player):
+                sys.exit("Game over!")
         screen.fill("black")
         for obj in drawable:
             obj.draw(screen)
